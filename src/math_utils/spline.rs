1b013fe1e917a3a0fbd0cb5e94383408c240b981
Adam Johansson <adam.johansson.144@gmail.com>
2026-01-02 16:16:09 +0100

    Implemented arc length computation on Bezier curves

--- src/math_utils/spline.rs
+++ src/math_utils/spline.rs
@@ -1,11 +1,11 @@
 use super::vec::Vec2;
 use itertools::Itertools;
 
+use std::cell::OnceCell;
+
 
 // TODO: 
 //   - Given a point, find the closest point (or its parameter) on the spline
-//   - Given a parameter, find the arc length
-//   - Given an arc length, find the parameter
 
 
 pub struct CubicBezier {
@@ -16,6 +16,7 @@ pub struct CubicBezier {
     c1: Vec2<f32>,
     c2: Vec2<f32>,
     c3: Vec2<f32>,
+    arc_length: OnceCell<f32>,
 }
 
 
@@ -42,7 +43,8 @@ impl CubicBezier {
         let c3 = -start + p1*3.0 - p2*3.0 + end;
         CubicBezier {
             start, p1, p2, end,
-            c1, c2, c3
+            c1, c2, c3,
+            arc_length: OnceCell::new()
         }
     }
 
@@ -56,6 +58,25 @@ impl CubicBezier {
 
     pub fn tangent(&self, t: f32) -> Vec2<f32> {
         self.velocity(t).normalized()
+    }
+
+    fn _arc_length(&self, t_start: f32, t_end: f32, steps: usize) -> f32 {
+        // Arc length is int_{t_start}^{t_end} |velocity(t)|dt
+        // Compute it numerically using trapezoid method
+        let dt = (t_end - t_start) / steps as f32;
+        let ts = (1 .. steps).map(|i| t_start + i as f32*dt);  // [dt, 2*dt, ..., t-dt]
+                                                               //
+        ts.map(|t| self.velocity(t).norm()*dt).sum::<f32>() + 0.5*dt*(self.velocity(t_start).norm() + self.velocity(t_end).norm())
+    }
+
+    // Computes the tangential arc length from t=0 to t=t
+    pub fn arc_length(&self, t: f32) -> f32 {
+        if t == 1.0 {
+            *self.arc_length.get_or_init(|| self._arc_length(0.0, 1.0, 64))
+            
+        } else {
+            self._arc_length(0.0, t, 64)
+        }
     }
 }
 
@@ -76,36 +97,39 @@ impl SmoothBezierSpline {
         Self { segments, max_u }
     }
 
-    pub fn get(&self, u: f32) -> Vec2<f32> {
+    fn segment_and_t(&self, u: f32) -> (&CubicBezier, usize, f32) {
+        // Edge case were rounding would give index error otherwise
+        if u >= self.max_u {
+            let i = self.segments.len() - 1;
+            return (&self.segments[i], i, 1.0);
+        }
         let u = u.min(self.max_u).max(0.0);
         let i = u as usize;
-        let t = u.fract();
+        (&self.segments[i], i, u.fract())
+    }
 
-        // Edge case 
-        if u == self.max_u {
-            return self.segments[self.segments.len()-1].end;
-        }
-
-        let segment = &self.segments[i];
+    pub fn get(&self, u: f32) -> Vec2<f32> {
+        let (segment, _, t) = self.segment_and_t(u);
         segment.get(t)
     }
 
     pub fn velocity(&self, u: f32) -> Vec2<f32> {
-        let u = u.min(self.max_u).max(0.0);
-        let i = u as usize;
-        let t = u.fract();
-
-        // Edge case 
-        if u == self.max_u {
-            return self.segments[self.segments.len()-1].velocity(1.0);
-        }
-
-        let segment = &self.segments[i];
+        let (segment, _, t) = self.segment_and_t(u);
         segment.velocity(t)
     }
 
     pub fn tangent(&self, u: f32) -> Vec2<f32> {
         self.velocity(u).normalized()
+    }
+
+    pub fn arc_length(&self, u: f32) -> f32 {
+        let (active_segment, i, t) = self.segment_and_t(u);
+
+        // All prior segments have the full length contribute
+        let previous_length: f32 = self.segments[0..i].iter().map(|segment| segment.arc_length(1.0)).sum();
+
+        // Arc length is prior length, plus the arc length on the active segment
+        previous_length + active_segment.arc_length(t)
     }
 }
 
@@ -179,6 +203,27 @@ mod tests {
         assert_eq!(spline.tangent(0.5), Vec2(1.0, 0.0));
         assert_eq!(spline.tangent(1.0), Vec2(0.0, -1.0));
         assert_eq!(spline.tangent(2.0), Vec2(0.0, 1.0));
+    }
+
+    #[test]
+    fn test_arclength() {
+        // Test arc length computation on a simple straight line Bezier curve
+
+        let spline = CubicBezier::new(Vec2(0.0, 0.0), Vec2(4.0, 3.0), Vec2(8.0, 6.0), Vec2(12.0, 9.0));
+        assert_eq!(spline.arc_length(0.0), 0.0);
+        assert!(spline.arc_length(1.0 / 3.0) > 4.99);
+        assert!(spline.arc_length(1.0 / 3.0) < 5.01);
+        assert!(spline.arc_length(2.0 / 3.0) > 9.99);
+        assert!(spline.arc_length(2.0 / 3.0) < 10.01);
+        assert!(spline.arc_length(1.0) > 14.99);
+        assert!(spline.arc_length(1.0) < 15.01);
+
+        let spline = SmoothBezierSpline::new(vec![BezierControl{ point: Vec2(0.0, 0.0), velocity: Vec2(4.0, 3.0)},
+                                                  BezierControl{ point: Vec2(12.0, 9.0), velocity: Vec2(4.0, 3.0)},
+                                                  BezierControl{ point: Vec2(24.0, 18.0), velocity: Vec2(4.0, 3.0)}]);
+        assert!(spline.arc_length(1.0 / 3.0) > 4.99);
+        assert!(spline.arc_length(1.0 / 3.0) < 5.01);
+        assert_eq!(spline.arc_length(1.0 + 1.0 / 3.0), 20.0);
     }
 }
 
